5bec8c4f1a7a60225cc9edf9552af8337f66f8be
Anemone95 <x565178035@126.com>
2020-11-09 15:49:49 +0800

    20/11/09 拆分规则和引擎

--- wala-taint/src/test/java/com/ibm/wala/examples/SOAP2020/taint/PythonWalaTaintAnalysisTest.java
+++ wala-taint/src/test/java/com/ibm/wala/examples/SOAP2020/taint/PythonWalaTaintAnalysisTest.java
@@ -2,22 +2,36 @@ package com.ibm.wala.examples.SOAP2020.taint;
 
 import com.ibm.wala.cast.python.client.PythonAnalysisEngine;
 import com.ibm.wala.cast.python.loader.PythonLoaderFactory;
+import com.ibm.wala.cast.python.types.PythonTypes;
 import com.ibm.wala.cast.python.util.PythonInterpreter;
 import com.ibm.wala.cast.util.SourceBuffer;
+import com.ibm.wala.classLoader.CallSiteReference;
 import com.ibm.wala.classLoader.Module;
-import com.ibm.wala.classLoader.SourceFileModule;
 import com.ibm.wala.classLoader.SourceURLModule;
+import com.ibm.wala.ipa.callgraph.CGNode;
+import com.ibm.wala.ipa.callgraph.CallGraph;
+import com.ibm.wala.ipa.callgraph.propagation.AllocationSiteInNode;
+import com.ibm.wala.ipa.callgraph.propagation.InstanceKey;
+import com.ibm.wala.ipa.callgraph.propagation.LocalPointerKey;
+import com.ibm.wala.ipa.callgraph.propagation.PointerAnalysis;
+import com.ibm.wala.ipa.slicer.NormalStatement;
+import com.ibm.wala.ipa.slicer.ParamCaller;
+import com.ibm.wala.ipa.slicer.Statement;
+import com.ibm.wala.ssa.SSAGetInstruction;
+import com.ibm.wala.ssa.SSAInstruction;
+import com.ibm.wala.types.Descriptor;
+import com.ibm.wala.types.MethodReference;
+import com.ibm.wala.types.Selector;
+import com.ibm.wala.types.TypeReference;
+import com.ibm.wala.util.intset.OrdinalSet;
+import com.ibm.wala.util.strings.Atom;
 import magpiebridge.core.AnalysisConsumer;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.Collection;
 import java.util.Collections;
-
-import static org.junit.Assert.*;
 
 public class PythonWalaTaintAnalysisTest {
 
